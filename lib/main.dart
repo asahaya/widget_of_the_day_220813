224370a214cad3940aea70b624edc4b3a556d1df
asahaya <asa7kadensony@gmail.com>
2022-08-13 23:02:49 +0900

    no03_column

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:widget_of_the_day_220813/no02_Expanded.dart';
+import 'package:widget_of_the_day_220813/no03_Column_row.dart';
 
 import 'no01_Cointainer.dart';
 
 void main() {
-  runApp(const No02_Expanded());
+  runApp(const No03_Column_Row());
 }
