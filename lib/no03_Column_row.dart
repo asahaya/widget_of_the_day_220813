41e41e8a7a01b3df3769411bfe72db58a966858b
asahaya <asa7kadensony@gmail.com>
2022-08-13 23:06:29 +0900

    crossAxisAlignment

--- lib/no03_Column_row.dart
+++ lib/no03_Column_row.dart
@@ -15,21 +15,23 @@ class _No03_Column_RowState extends State<No03_Column_Row> {
       home: Scaffold(
         body: Column(
           //Column向きに全体を中央揃え
-          // mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
 
           // mainAxisAlignment: MainAxisAlignment.start, //上
           // mainAxisAlignment: MainAxisAlignment.end, //下
-          mainAxisAlignment: MainAxisAlignment.spaceAround, //等間隔分け（上下余白あり）
+          // mainAxisAlignment: MainAxisAlignment.spaceAround, //等間隔分け（上下余白あり）
           // mainAxisAlignment: MainAxisAlignment.spaceBetween, //等間隔分け（上下余白なし）
           // mainAxisAlignment: MainAxisAlignment.spaceEvenly, //等間隔分け（上下余白あり）
+          //Columnと逆向きの左揃え
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              width: 100,
+              // width: 100,
               height: 100,
               color: Colors.deepPurple[600],
             ),
             Container(
-              width: 100,
+              width: 300,
               height: 100,
               color: Colors.deepPurple[400],
             ),
