90612b0ad2d877c5d0acd15558437a671afffa63
asahaya <asa7kadensony@gmail.com>
2022-08-14 11:07:12 +0900

    no07_GridView.builder

--- lib/no07_GridView_Builder.dart
+++ lib/no07_GridView_Builder.dart
@@ -12,37 +12,21 @@ class _No07_GridView_BuilderState extends State<No07_GridView_Builder> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: GridView.count(
-          //横に並べる数
-          crossAxisCount: 2,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 50,
-                width: 50,
-                color: Colors.deepPurple,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 50,
-                width: 50,
-                color: Colors.deepPurple,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 50,
-                width: 50,
-                color: Colors.deepPurple,
-              ),
-            ),
-          ],
-        ),
-      ),
+          body: GridView.builder(
+              //配列する個数
+              itemCount: 5,
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.deepPurple,
+                  ),
+                );
+              })),
     );
   }
 }
