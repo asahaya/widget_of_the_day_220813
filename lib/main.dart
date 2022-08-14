d0fcc826f5b2fc54bf74a19043f1c798769baf40
asahaya <asa7kadensony@gmail.com>
2022-08-14 15:34:49 +0900

    no12_sliverappber

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,11 @@ import 'package:widget_of_the_day_220813/no07_GridView_Builder.dart';
 import 'package:widget_of_the_day_220813/no09_BottomNavigationBar.dart';
 import 'package:widget_of_the_day_220813/no10_AppBar.dart';
 import 'package:widget_of_the_day_220813/no11_Drawer.dart';
+import 'package:widget_of_the_day_220813/no12_Sliver_AppBer.dart';
 
 import 'no01_Cointainer.dart';
 import 'no08_GestureDetector.dart';
 
 void main() {
-  runApp(const No11_Drawer());
+  runApp(const No12_SliverAppBar());
 }
--- lib/no12_Sliver_AppBer.dart
+++ lib/no12_Sliver_AppBer.dart
@@ -1,24 +1,69 @@
 import 'package:flutter/material.dart';
 
-
-class No01_Container extends StatefulWidget {
-  const No01_Container({Key? key}) : super(key: key);
+class No12_SliverAppBar extends StatefulWidget {
+  const No12_SliverAppBar({Key? key}) : super(key: key);
 
   @override
-  State<No01_Container> createState() => _No01_ContainerState();
+  State<No12_SliverAppBar> createState() => _No12_SliverAppBarState();
 }
 
-class _No01_ContainerState extends State<No01_Container> {
+class _No12_SliverAppBarState extends State<No12_SliverAppBar> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(''),
-          backgroundColor: Colors.amber,
+      home: Separate(),
+    );
+  }
+}
+
+class Separate extends StatefulWidget {
+  const Separate({Key? key}) : super(key: key);
+
+  @override
+  State<Separate> createState() => _SeparateState();
+}
+
+class _SeparateState extends State<Separate> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: CustomScrollView(slivers: [
+        SliverAppBar(
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple[300],
+          leading: Icon(Icons.menu),
+          title: Text(
+            'SLIVERAPPBER',
+            style: TextStyle(letterSpacing: 10.0),
+          ),
+          expandedHeight: 250,
         ),
-        body: Container(),
-      ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(40),
+              child: Container(
+                height: 400,
+                color: Colors.deepPurple[200],
+              ),
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(40),
+              child: Container(
+                height: 400,
+                color: Colors.deepPurple[200],
+              ),
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
