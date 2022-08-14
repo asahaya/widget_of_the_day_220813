6496febd08575810c5603a2523fb7467b8d8cb4b
asahaya <asa7kadensony@gmail.com>
2022-08-14 10:46:32 +0900

    crip

--- lib/no06_ImageAsset.dart
+++ lib/no06_ImageAsset.dart
@@ -14,19 +14,22 @@ class _No06_ImageAssetState extends State<No06_ImageAsset> {
       home: Scaffold(
         backgroundColor: Colors.grey,
         body: Center(
-            child: Container(
-                height: 300,
-                width: 300,
-                color: Colors.amber,
-                child: Image.asset(
-                  'assets/images/flutter_image.png',
-                  //contain=画面サイズに合わせて拡大する
-                  //cover=余白をなくして広げる
-                  //fitWidth=横幅に広げる
-                  //fitHeight=縦幅に広げる
-                  //fill=比率を無視して塗りつぶす
-                  fit: BoxFit.fill,
-                ))),
+            child: ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: Container(
+              height: 300,
+              width: 300,
+              color: Colors.amber,
+              child: Image.asset(
+                'assets/images/flutter_image.png',
+                //contain=画面サイズに合わせて拡大する
+                //cover=余白をなくして広げる
+                //fitWidth=横幅に広げる
+                //fitHeight=縦幅に広げる
+                //fill=比率を無視して塗りつぶす
+                fit: BoxFit.fill,
+              )),
+        )),
       ),
     );
   }
