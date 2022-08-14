e2ee5724ef3166dd9259a2e28ea0f6a5753617a8
asahaya <asa7kadensony@gmail.com>
2022-08-14 17:21:13 +0900

    no14_animationContainer_amimation_alignment

--- lib/no14_Animated_container.dart
+++ lib/no14_Animated_container.dart
@@ -12,6 +12,8 @@ class _No14_AnimatedContainerState extends State<No14_AnimatedContainer> {
   double boxHeight = 100;
   double boxWidth = 100;
   var boxColor = Colors.deepPurple;
+  double boxX = -1;
+  double boxY = -1;
 
   void _expandedBox() {
     setState(() {
@@ -21,21 +23,32 @@ class _No14_AnimatedContainerState extends State<No14_AnimatedContainer> {
     });
   }
 
+  void _moveBox() {
+    setState(() {
+      boxX = 1;
+      boxY = 1;
+      print("duration!!!!!!");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: GestureDetector(
-        onTap: _expandedBox,
+        onTap: _moveBox,
         child: Scaffold(
           backgroundColor: Colors.deepPurple[200],
-          body: Center(
-              child: AnimatedContainer(
+          body: AnimatedContainer(
             //1秒で変化完了
             duration: Duration(seconds: 1),
-            height: boxHeight,
-            width: boxWidth,
-            color: boxColor,
-          )),
+            alignment: Alignment(boxX, boxY),
+            //Containerで内包しないと実行されない？？
+            child: Container(
+              height: boxHeight,
+              width: boxWidth,
+              color: boxColor,
+            ),
+          ),
         ),
       ),
     );
