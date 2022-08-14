3fb209a5684730c8664b26a3beffb63848106c2b
asahaya <asa7kadensony@gmail.com>
2022-08-14 10:45:23 +0900

    boxfit

--- lib/no06_ImageAsset.dart
+++ lib/no06_ImageAsset.dart
@@ -12,7 +12,21 @@ class _No06_ImageAssetState extends State<No06_ImageAsset> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(child: Image.asset('assets/images/flutter_image.png')),
+        backgroundColor: Colors.grey,
+        body: Center(
+            child: Container(
+                height: 300,
+                width: 300,
+                color: Colors.amber,
+                child: Image.asset(
+                  'assets/images/flutter_image.png',
+                  //contain=画面サイズに合わせて拡大する
+                  //cover=余白をなくして広げる
+                  //fitWidth=横幅に広げる
+                  //fitHeight=縦幅に広げる
+                  //fill=比率を無視して塗りつぶす
+                  fit: BoxFit.fill,
+                ))),
       ),
     );
   }
