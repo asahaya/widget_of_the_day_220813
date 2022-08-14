7c7abdc0063a4b3ce089d7e7aa3f7098b7097196
asahaya <asa7kadensony@gmail.com>
2022-08-14 16:55:44 +0900

    no14_animationContainer_amimation

--- lib/no14_Animated_container.dart
+++ lib/no14_Animated_container.dart
@@ -11,11 +11,13 @@ class _No14_AnimatedContainerState extends State<No14_AnimatedContainer> {
   //初期値
   double boxHeight = 100;
   double boxWidth = 100;
+  var boxColor = Colors.deepPurple;
 
   void _expandedBox() {
     setState(() {
       boxHeight = 300;
       boxWidth = 300;
+      boxColor = Colors.pink;
     });
   }
 
@@ -27,10 +29,12 @@ class _No14_AnimatedContainerState extends State<No14_AnimatedContainer> {
         child: Scaffold(
           backgroundColor: Colors.deepPurple[200],
           body: Center(
-              child: Container(
+              child: AnimatedContainer(
+            //1秒で変化完了
+            duration: Duration(seconds: 1),
             height: boxHeight,
             width: boxWidth,
-            color: Colors.deepPurple,
+            color: boxColor,
           )),
         ),
       ),
