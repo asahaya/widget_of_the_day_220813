fcf044b97dde156ce84f378fcb24fb4022bf1f71
asahaya <asa7kadensony@gmail.com>
2022-08-14 10:05:00 +0900

    ClipRRect

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:widget_of_the_day_220813/no02_Expanded.dart';
 import 'package:widget_of_the_day_220813/no03_Column_row.dart';
 import 'package:widget_of_the_day_220813/no04_ListView_Builder.dart';
+import 'package:widget_of_the_day_220813/no05_Cliprrest_RoundCorners.dart';
 
 import 'no01_Cointainer.dart';
 
 void main() {
-  runApp(const No04_ListView_Builder());
+  runApp(const No05_RoundCorners());
 }
