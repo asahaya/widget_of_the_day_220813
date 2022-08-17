b869e51679c944b0c073e08c9a701fbf48ffaef4
asahaya <asa7kadensony@gmail.com>
2022-08-17 23:36:43 +0900

    a02_dragAnddrop_IgnorePointer

--- lib/a_no02_Drag_and_Drop.dart
+++ lib/a_no02_Drag_and_Drop.dart
@@ -24,33 +24,44 @@ class _a02SeparateState extends State<a02Separate> {
 
   @override
   Widget build(BuildContext context) {
+    final showDraggable = color == Colors.black;
     return Scaffold(
       body: Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          DragTarget(
+          DragTarget<Color>(
+            onAccept: (data) => setState(() => color = data),
             builder: (context, _, __) => Container(
               color: color,
               width: 200,
               height: 200,
             ),
           ),
-          Draggable(
-            child: Container(
-              color: Colors.green,
-              width: 200,
-              height: 200,
-            ),
-            feedback: Container(
-              color: Colors.orange,
-              width: 200,
-              height: 200,
-            ),
-            childWhenDragging: Container(
-              color: Colors.red,
-              width: 200,
-              height: 200,
+
+          //移動すると元の図形が消える
+          IgnorePointer(
+            ignoring: !showDraggable,
+            child: Opacity(
+              opacity: showDraggable ? 1 : 0,
+              child: Draggable<Color>(
+                data: Colors.green,
+                child: Container(
+                  color: Colors.green,
+                  width: 200,
+                  height: 200,
+                ),
+                feedback: Container(
+                  color: Colors.orange,
+                  width: 200,
+                  height: 200,
+                ),
+                childWhenDragging: Container(
+                  color: Colors.red,
+                  width: 200,
+                  height: 200,
+                ),
+              ),
             ),
           )
         ],
